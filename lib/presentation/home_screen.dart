53642d0d1aad8b6b9edb3b2ccba5e6b8afecece6
Patrik Pralas <patrik.pralas@q.agency>
2023-07-13 10:15:00 +0200

    Added log out button

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:init_test_task/common/authentication_notifier.dart';
+import 'package:init_test_task/common/models/not_authenticated.dart';
 import 'package:init_test_task/domain/notifiers/user_notifiers/user_state_notifier.dart';
 
 @RoutePage()
@@ -27,6 +29,13 @@ class HomePage extends ConsumerWidget {
                   orElse: () => 'error',
                   loaded: (user) => user.lastName,
                 ),
+              ),
+              ElevatedButton(
+                onPressed: () =>
+                    ref.read(authenticationNotifierProvider.notifier).signOut(
+                          NotAuthenticated(isAuthenticated: false),
+                        ),
+                child: const Text('Sign Out'),
               ),
             ],
           ),
